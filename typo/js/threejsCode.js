4d14d36dd97cef5839c94876610a7f828bacbf00
metalfinger <hir.012612@gmail.com>
2019-04-02 12:55:11 -0700

    put text

--- typo/js/threejsCode.js
+++ typo/js/threejsCode.js
@@ -142,7 +142,7 @@ material= new THREE.MeshPhongMaterial( {
 var texture = new THREE.TextureLoader().load( "tex.png" );
 var bMap = new THREE.TextureLoader().load( "bump.png" );
 
-// material = new THREE.MeshPhongMaterial( { map:texture, normalMap :bMap} );
+material = new THREE.MeshPhongMaterial( { map:texture, normalMap :bMap} );
 
 
 
