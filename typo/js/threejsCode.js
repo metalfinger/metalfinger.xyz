83096d345d4e6345128bc821d6a83be5e085abbf
metalfinger <hir.012612@gmail.com>
2019-04-03 10:11:20 -0700

    Try

--- typo/js/threejsCode.js
+++ typo/js/threejsCode.js
@@ -176,7 +176,7 @@ material= new THREE.MeshPhongMaterial( {
 
 
 var texture = new THREE.TextureLoader().load( "texB.png" );
-var bMap = new THREE.TextureLoader().load( "bumpB.png" );
+var bMap = new THREE.TextureLoader().load( "bump.png" );
 
 material = new THREE.MeshPhongMaterial( { map:texture, normalMap :bMap} );
 
