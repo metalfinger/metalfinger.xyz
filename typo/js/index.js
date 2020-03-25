2ba1ec1d8894b10be00bfcbb110804cdeed7cb5b
Hiren <hir.012612@gmail.com>
2020-03-25 18:32:39 +0530

    Added Instruction

--- typo/js/index.js
+++ typo/js/index.js
@@ -1,6 +1,8 @@
 var currentStage = 0;
 var maxStage = 20;
 
+var hideScrollText = false;
+
 
 var testElement = $('#containerID');
 function run(v) {
@@ -9,23 +11,31 @@ function run(v) {
         step: function (val) {
             testElement.css("transform", `translateY(${val}%)`);
         },
-        complete:  function() {
+        complete: function () {
             scrollAnimating = false;
-        } 
+        }
     })
 }
 
 var scrollAnimating = false;
 
 $('#mainBody').bind('mousewheel', function (e) {
-    console.log(e.originalEvent.wheelDelta+"      <------   WHEEL DELTA")
-    if(!scrollAnimating){
-    if (e.originalEvent.wheelDelta  > 2) 
-    {
-        moveDown();
-    } else if (e.originalEvent.wheelDelta  < -2) {
-        moveUp();
-    }}
+    console.log(e.originalEvent.wheelDelta + "      <------   WHEEL DELTA")
+
+
+    if (!scrollAnimating) {
+        if (e.originalEvent.wheelDelta > 2) {            
+            moveDown();
+        } else if (e.originalEvent.wheelDelta < -2) {
+            console.log("trigger");
+            if (!hideScrollText) {
+                hideScrollText = true;
+
+                document.getElementById("scroll-metalfinger").style.opacity = "0.0";
+            }
+            moveUp();
+        }
+    }
 });
 
 // MOVE UP FUNCTION
@@ -63,50 +73,50 @@ function moveDown() {
 
 
 //!FOR MOBILE DEVICES
-document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchstart', handleTouchStart, false);
 document.addEventListener('touchmove', handleTouchMove, false);
 
-var xDown = null;                                                        
+var xDown = null;
 var yDown = null;
 
 function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
 
 function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
 
 function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
+    if (!xDown || !yDown) {
         return;
     }
 
-    var xUp = evt.touches[0].clientX;                                    
+    var xUp = evt.touches[0].clientX;
     var yUp = evt.touches[0].clientY;
 
     var xDiff = xDown - xUp;
     var yDiff = yDown - yUp;
 
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* left swipe */ 
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        if (xDiff > 0) {
+            /* left swipe */
         } else {
             /* right swipe */
-        }                       
+        }
     } else {
-        if ( yDiff > 0 ) {
-            /* up swipe */ 
+        if (yDiff > 0) {
+            /* up swipe */
             moveUp();
-        } else { 
+        } else {
             /* down swipe */
             moveDown();
-        }                                                                 
+        }
     }
     /* reset values */
     xDown = null;
-    yDown = null;                                             
+    yDown = null;
 };
