6858e45c963aa2d607790a63d9f81850ce954197
Hiren <hir.012612@gmail.com>
2020-03-25 17:37:07 +0530

    Swipe Test

--- typo/js/index.js
+++ typo/js/index.js
@@ -5,7 +5,7 @@ var maxStage = 20;
 var testElement = $('#containerID');
 function run(v) {
     $(v[0]).animate(v[1], {
-        duration: 500,
+        duration: 300,
         step: function (val) {
             testElement.css("transform", `translateY(${val}%)`);
         },
@@ -22,12 +22,8 @@ $('#mainBody').bind('mousewheel', function (e) {
     if(!scrollAnimating){
     if (e.originalEvent.wheelDelta  > 2) 
     {
-        console.log('DOWN');
-        event.preventDefault();
         moveDown();
     } else if (e.originalEvent.wheelDelta  < -2) {
-        event.preventDefault();
-        console.log('UP');
         moveUp();
     }}
 });
@@ -62,3 +58,55 @@ function moveDown() {
     run([{ y: -(perObject * currentStage) }, { y: -(perObject * newY) }])
     currentStage = newY;
 }
+
+
+
+
+//!FOR MOBILE DEVICES
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+        } else {
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+            moveUp();
+        } else { 
+            /* down swipe */
+            moveDown();
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
